6a89a7406d9dd0048d54f1e2c4c3c8c7551ea27c
Andre121314115 <71975050@continental.edu.pe>
2026-09-08 17:30:28 -0500

    Favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,8 +53,10 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.svg", sizes: "any" },
     ],
-    shortcut: "/favicon.svg",
+    shortcut: [{ url: "/favicon.svg", type: "image/svg+xml" }],
+    apple: [{ url: "/favicon.svg" }],
   }
 };
 
